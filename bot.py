76bd8da9f85254e5ab65b712465b16cb9027bf74
delano yoder <delano@advanced.farm>
2023-06-01 13:20:10 -0700

    [Feature] loop if pay info was entered incorrectly

--- bot.py
+++ bot.py
@@ -45,14 +45,14 @@ except:
 try:
     # fill in the fields
     shipping_fields = {
-        "checkout_email": "delanoayoder@gmail.com",  # replace with your email
-        "checkout_shipping_address_first_name": "Delano",  # replace with your first name
-        "checkout_shipping_address_last_name": "Yoder",  # replace with your last name
-        "checkout_shipping_address_address1": "532 Douglas St",  # replace with your address
-        "checkout_shipping_address_address2": "Unit 204",  # replace with your apartment, suite, etc.
-        "checkout_shipping_address_city": "West Sacramento",  # replace with your city
-        "checkout_shipping_address_zip": "95691",  # replace with your zip code
-        "checkout_shipping_address_phone": "14145732011",  # replace with your phone number
+        "checkout_email": "emaill@address.com",  # replace with your email
+        "checkout_shipping_address_first_name": "first",  # replace with your first name
+        "checkout_shipping_address_last_name": "last",  # replace with your last name
+        "checkout_shipping_address_address1": "address",  # replace with your address
+        "checkout_shipping_address_address2": "unit",  # replace with your apartment, suite, etc.
+        "checkout_shipping_address_city": "city",  # replace with your city
+        "checkout_shipping_address_zip": "12345",  # replace with your zip code
+        "checkout_shipping_address_phone": "1234567890",  # replace with your phone number
     }
 
     for id, input_text in shipping_fields.items():
@@ -89,33 +89,40 @@ try:
     ]
 
     credit_card_input_text = [
-        "1234567812345678",
-        "Delano Yoder",
-        "0926",
-        "114",
+        "0123456789101213",
+        "name",
+        "1234",
+        "123",
     ]
 
-    # Switch to the iframe
-    for i, iframe in enumerate(iframes):
-        driver.switch_to.frame(iframe)
+    while True:
+        pay_now_btn_exists = driver.find_elements(By.CSS_SELECTOR, ".step__footer__continue-btn.btn")
+        if pay_now_btn_exists:
+            # Switch to the iframe
+            for i, iframe in enumerate(iframes):
+                driver.switch_to.frame(iframe)
 
-        wait = WebDriverWait(driver, 20)
-        input_box = wait.until(EC.element_to_be_clickable((By.ID, credit_card_ids[i])))
-        input_box.clear()
-        time.sleep(0.1)
-        input_box.send_keys(credit_card_input_text[i])
+                wait = WebDriverWait(driver, 20)
+                input_box = wait.until(EC.element_to_be_clickable((By.ID, credit_card_ids[i])))
+                input_box.clear()
+                time.sleep(0.1)
+                input_box.send_keys(credit_card_input_text[i])
 
-        # Switch back to the main content when finished
-        driver.switch_to.default_content()
+                # Switch back to the main content when finished
+                driver.switch_to.default_content()
 
-    time.sleep(500)
-
-    pay_now_btn = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, ".step__footer__continue-btn.btn"))
-    )
-    pay_now_btn.click()
+            pay_now_btn = WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, ".step__footer__continue-btn.btn"))
+            )
+            pay_now_btn.click()
+        else:
+            print("Payment successful!")
+            break
 
 finally:
     # always close the browser to free up resources
-    time.sleep(600)
+    while True:
+        user_input = input("Enter `q` to quit: ")
+        if user_input == 'q':
+            break
     driver.quit()
